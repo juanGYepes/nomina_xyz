5db5577e41651e5dec115dd00dd29f36f212a2ed
juanGYepes <juancyepes97@gmail.com>
2025-03-15 09:41:11 -0500

    us1/create register

---
+++ nomina.py
@@ -0,0 +1,31 @@
+## US1/create register
+
+##Matriz de usuarios
+users = []
+
+def createUser():
+    user = []
+    id = int(input("Ingrese su documento de identidad: "))
+    user.append(id)
+    user_name = input("Ingrese su nombre: ")
+    user.append(user_name)
+    user_last_name = input("Ingrese su apellido: ")
+    user.append(user_last_name)
+    phone = input("Ingrese su telefono: ")
+    user.append(phone)
+    user_email = input("Ingrese su correo electronico: ")
+    user.append(user_email)
+    user_password = input("Ingrese su contraseña: ")
+    user.append(user_password)
+    users.append(user)
+
+createUser()
+createUser()
+
+print(users)
+
+
+
+
+    
+
